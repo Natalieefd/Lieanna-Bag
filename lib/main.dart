1348fdd294419136169d8b130af9af65f97f8d7e
Sherina Laraswati <93170060+SherinaLaraswati@users.noreply.github.com>
2023-11-14 15:41:02 +0800

    home-screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lienna_bag/firebase_options.dart';
+import 'package:lienna_bag/page/home_screen.dart';
 import 'package:lienna_bag/page/login_page.dart';
 
 void main() async {
@@ -22,8 +23,30 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+            fontWeight: FontWeight.bold
+          ),
+
+          titleLarge: TextStyle(
+            fontSize: 20,
+            color: Colors.black
+          ),
+
+          titleMedium: TextStyle(
+            fontSize: 18,
+              color: Colors.black
+          ),
+
+          titleSmall: TextStyle(
+            fontSize: 15,
+              color: Colors.black
+          )
+        )
       ),
-      home: LoginPage(),
+      home: hom_scrn(),
     );
   }
 }
---
+++ lib/page/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class hom_scrn extends StatefulWidget {
+  const hom_scrn({super.key});
+
+  @override
+  State<hom_scrn> createState() => _hom_scrnState();
+}
+
+class _hom_scrnState extends State<hom_scrn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        
+        scrollDirection: Axis.horizontal,
+        children: [
+          Row(
+            children:[
+              Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  // image: DecorationImage(
+                  //   image: AssetImage("Assets/bracelets.jpg"),
+                  //   fit: BoxFit.cover)
+                ),
+              ),
+              Text("Welcome, User A!")
+            ]
+          )
+        ],
+
+      ),
+    );
+  }
+}
