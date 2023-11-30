77be8d87313b0773c64b12ab2c4a67d38fbd1fd4
rag <92092729+kigungdhon@users.noreply.github.com>
2023-11-30 21:08:48 +0800

    feat: new page(favorite & merk)
    UI Kasaran (belum finishing)

---
+++ lib/page/merk_page.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MerkPage extends StatefulWidget {
+  const MerkPage({super.key});
+
+  @override
+  State<MerkPage> createState() => _MerkPageState();
+}
+
+class _MerkPageState extends State<MerkPage> {
+  @override
+  void initState() {
+    getClientStream();
+    super.initState();
+  }
+
+  List _allResults = [];
+
+  void getClientStream() async {
+    var data = await FirebaseFirestore.instance
+        .collection('tas')
+        .orderBy('nama')
+        .get();
+
+    setState(() {
+      _allResults = data.docs;
+    });
+  }
+
+  String getFirstSentence(String text) {
+    List<String> sentences = text.split(RegExp(r'[.!? ]'));
+    if (sentences.isNotEmpty) {
+      return sentences.first.trim();
+    } else {
+      return text;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'MERK A',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w600,
+            height: 0,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: orientation == Orientation.portrait ? 2 : 3, 
+          crossAxisSpacing: 20.0, 
+          mainAxisSpacing: 20.0, 
+        ),
+        itemCount: _allResults.length,
+        padding: const EdgeInsets.all(20),
+        itemBuilder: (context, index) {
+          final item = _allResults[index];
+          return Column(
+            children: <Widget>[
+              Container(
+                width: 111,
+                height: 124,
+                decoration: ShapeDecoration(
+                  color: const Color(0xFFD9D9D9),
+                  image: DecorationImage(
+                    image: NetworkImage(item['gambar']),
+                    repeat: ImageRepeat.repeat
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              Expanded(child: Text(getFirstSentence(item['nama'].toUpperCase())))
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
